10f2b95add4e89c9c3b05d16a0821df2670f1385
ludivine_maechler <ludivine.maechler@cantico.fr>
2017-03-31 10:22:32 +0200

    first commit

---
+++ src/components/TodoList.js
@@ -0,0 +1,31 @@
+import React, {PropTypes} from 'react';
+import TodoItem from "./TodoItem";
+
+const TodoList = (props) => {
+  return <ul onDragOver={this.dragOver}>
+  {
+    props.todos.map((item, index)=>{
+
+      const isTodoFiltered = (props.filter==="todo" && item.complete===true);
+      const isCompleteFiltered = (props.filter==="complete" && item.complete===false);
+      if(isTodoFiltered || isCompleteFiltered) return null;
+
+      return <TodoItem key={index}
+                       label={item.label}
+                       complete={item.complete}
+                       onClick={props.selectItem.bind(this,index)}
+                       draggable="true"
+                       onDragEnd={item.dragEnd}
+                       onDragStart={item.dragStart}
+            />
+    })
+  }</ul>;
+}
+
+TodoList.propTypes = {
+  todos: PropTypes.array.isRequired,
+  filter: PropTypes.string.isRequired,
+  selectItem: PropTypes.func.isRequired,
+}
+
+export default TodoList;
